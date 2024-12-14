2dcd748dd1344ca3977bcced5826683561638d94
Hamza AFIF <127676587+HamzaAfif@users.noreply.github.com>
2024-12-14 11:28:29 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,9 +42,9 @@ var finalRoundhere = false;
 app.use(express.static(publicPath));
 
 //Starting server on port 3000
-server.listen(3000, () => {    
+server.listen(3000, '0.0.0.0', () => {    
     console.log("Server started on port 3000");
-});    
+});
 
 instrument(io, {auth: false})
 
